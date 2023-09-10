d40e5bb1812c819dde973d0e633aac510aeba75a
ali.abukahil <ali.abukahil@gmail.com>
2023-09-10 16:15:12 +0200

    Styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,35 +49,58 @@ const pizzaData = [
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
-    </>
+    </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  // const style = {
+  //   color: "purple",
+  //   fontSize: "48px",
+  //   textTransform: "uppercase",
+  // };
+
+  const style = {};
+
+  return (
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
-    </>
+    </main>
   );
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  /*   if (hour >= openHour && hour <= closeHour) {
+    alert("We’re currently open!");
+  } else alert("We'are closed!"); */
+
   // Writing a component without JSX
   //return React.createElement("footer", null, "We're currently open!");
   // with JSX
   return (
-    <footer>{new Date().toLocaleTimeString()}."We're currently open!"</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}."We're currently open!"
+    </footer>
   );
 }
 
@@ -84,7 +108,7 @@ function Pizza() {
   return (
     <>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, and pepperoni</p>
     </>
   );
